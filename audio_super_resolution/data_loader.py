d03d75b69636fc8ace99fe931760744706da04e4
hajix <sergioajini@gmail.com>
2021-03-22 09:08:37 -0400

    add additive augmentation

--- audio_super_resolution/data_loader.py
+++ audio_super_resolution/data_loader.py
@@ -10,12 +10,23 @@ from tqdm import tqdm
 
 
 class AudioLoader(Dataset):
-    def __init__(self, base_path, json_file_list, n_samples, n_mask):
+    def __init__(
+            self,
+            base_path,
+            json_file_list,
+            n_samples,
+            n_mask,
+            noise_file=None
+    ):
         self.base_path = base_path
         with open(json_file_list) as f:
             self.data = json.load(f)
         self.n_samples = n_samples
         self.n_mask = n_mask
+        if noise_file:
+            self.noise = sf.read(noise_file)
+        else:
+            self.noise = None
 
     def __len__(self):
         return len(self.data)
@@ -42,6 +53,10 @@ class AudioLoader(Dataset):
         if np.random.random() < .5:
             x = self._freq_mask(x, sr)
 
+        # additive noise
+        if np.random.random() < .5:
+            x = self._add_noise(x)
+
         x = np.ascontiguousarray(x, dtype=np.float32)
         x = torch.from_numpy(x).unsqueeze(0)
         y = torch.from_numpy(y).unsqueeze(0)
@@ -54,10 +69,25 @@ class AudioLoader(Dataset):
         return x
 
     def _freq_mask(self, x, sr):
-        low = np.random.randint(40, 60)
+        low = np.random.randint(40, 100)
         high = np.random.randint(3500, 4000)
         sos = butter(10, [low, high], 'bp', fs=sr, output='sos')
         x = sosfiltfilt(sos, x)
+        return x
+
+    def _add_noise(self, x):
+        if self.noise is None:
+            return x
+        noise_len = self.noise.shape[0]
+        start = np.random.randint(0, noise_len - self.n_samples)
+        end = start + self.n_samples
+        noise = self.noise[start:end]
+        # tune SNR
+        noise_snr = np.random.uniform(5, 20)
+        x_db = 10 * np.log10(np.mean(x ** 2) + 1e-4)
+        noise_db = 10 * np.log10(np.mean(noise ** 2) + 1e-4)
+        noise *= np.sqrt(10 ** ((x_db - noise_db - noise_snr) / 10))
+        x += noise
         return x
 
 
