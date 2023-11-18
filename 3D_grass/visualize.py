7f079ee30892cb02d9d344699e773cc748370c5c
mohamad-hasan-sohan-ajini <mohamad.hasan.sohan.ajini@gmail.com>
2023-11-19 03:04:11 +0330

    Speed up the algorithm

--- 3D_grass/visualize.py
+++ 3D_grass/visualize.py
@@ -10,7 +10,7 @@ pv.start_xvfb()
 # List of .obj file paths
 base_path = Path('results')
 obj_files = sorted(base_path.rglob('*.obj'))
-obj_files = obj_files[:10_000]
+obj_files = obj_files[:20_000]
 
 # Create a PyVista plotter
 plotter = pv.Plotter(
@@ -19,29 +19,27 @@ plotter = pv.Plotter(
     off_screen=True,
 )
 
+# Set up plotter options (if needed)
+plotter.set_background('black')
+# plotter.show_axes()
+
 # Load and add each object to the plotter
+print('Loaindg meshes...')
 green_texture = pv.Texture('Tex/GrassG.png')
 white_texture = pv.Texture('Tex/GrassW.png')
+max_x, sup_y, max_z = 0, 0, 0
 for obj_file in tqdm(obj_files, file=sys.stdout):
     mesh = pv.read(obj_file)
+    max_x = max(mesh.bounds[1], max_x)
+    sup_y = min(mesh.bounds[3], sup_y)
+    max_z = max(mesh.bounds[5], max_z)
     if '-G' in obj_file.name:
         plotter.add_mesh(mesh, texture=green_texture)
     else:
         plotter.add_mesh(mesh, texture=white_texture)
 
-# Set up plotter options (if needed)
-plotter.set_background('black')
-plotter.show_axes()
-
-# find bounds
-bounds = np.array([mesh.bounds for mesh in tqdm(plotter.meshes, file=sys.stdout)])
-bounds_max = bounds.max(axis=0)
-max_x = bounds_max[1]
-sup_y = bounds_max[2]
-max_z = bounds_max[5]
-
 # moving camera settings
-timesteps = 200
+timesteps = 300
 start_position = np.array([max_x, sup_y, max_z / 2])
 mid_position = np.array([max_x / 2, 1, max_z / 2])
 end_position = np.array([max_x / 2, 5, max_z / 2])
@@ -66,6 +64,7 @@ look_at_linspace1 = np.convolve(look_at_linspace[:, 1], np.ones(kernel_size), 'v
 look_at_linspace2 = np.convolve(look_at_linspace[:, 2], np.ones(kernel_size), 'valid') / kernel_size
 look_at_linspace = np.stack([look_at_linspace0, look_at_linspace1, look_at_linspace2], axis=1)
 
+print('Rendering...')
 for t, (position, look_at) in enumerate(zip(position_linspace, tqdm(look_at_linspace, file=sys.stdout))):
     # set up camera
     plotter.camera.position = position
