22fe60dc816116e7afc530fb1c52fe5e08992240
mohamad-hasan-sohan-ajini <mohamad.hasan.sohan.ajini@gmail.com>
2024-07-09 07:08:23 +0330

    Rectify visualization error

--- 3D_flag/visualize.py
+++ 3D_flag/visualize.py
@@ -29,23 +29,30 @@ plotter.camera_set = True
 
 
 # create faces array, only for z=0
-NUM_X_POINTS = 3
-NUM_Y_POINTS = 2
+NUM_X_POINTS = 100
+NUM_Y_POINTS = 70
 NUM_Z_POINTS = 1
 faces = []
 for j in range(NUM_Y_POINTS - 1):
     for i in range(NUM_X_POINTS - 1):
-        faces.append([2, i + j * NUM_X_POINTS, i + 1 + j * NUM_X_POINTS, i + NUM_X_POINTS + j * NUM_X_POINTS])
-        faces.append([2, i + 1 + j * NUM_X_POINTS, i + 1 + NUM_X_POINTS + j * NUM_X_POINTS, i + NUM_X_POINTS + j * NUM_X_POINTS])
-faces = np.array(faces, dtype=np.int32) + 1
-print(faces)
-# from IPython import embed
-# embed()
+        # p2----p1
+        # |      |
+        # |      |
+        # p3----p0
+        p0 = j + i * NUM_Y_POINTS
+        p1 = p0 + 1
+        p2 = p1 + NUM_Y_POINTS
+        p3 = p2 - 1
+        faces.append([3, p0, p1, p2])
+        faces.append([3, p0, p2, p3])
+faces = np.array(faces, dtype=np.int32)
 
 for t, position_pt in enumerate(tqdm(positions)):
     plotter.clear()
     position = torch.load(position_pt).view(-1, 3).cpu().numpy()
-    # mesh = pv.PolyData(position, faces)
+    # mesh_ = pv.PolyData(position)
+    # mesh_ = mesh_.rotate_x(90)
+    # plotter.add_mesh(mesh_, show_edges=True)
     mesh = pv.PolyData(position, faces=faces)
     mesh = mesh.rotate_x(90)
     plotter.add_mesh(mesh, show_edges=True)
@@ -53,9 +60,9 @@ for t, position_pt in enumerate(tqdm(positions)):
     # Display the rendering scene
     image = plotter.screenshot(f'/tmp/zrender{t:04d}.png')
 
-    print(position)
-    if t == 10:
-        break
+    # print(position)
+    # if t == 10:
+    #     break
 
 # from IPython import embed
 # embed()
